202e0e76c5ae0bab942abc2f664e93e254ea5275
mattiapomelli <mattiapomelli@gmail.com>
2023-05-14 00:24:21 +0100

    Add lenschool logo

--- src/components/basic/logo.tsx
+++ src/components/basic/logo.tsx
@@ -1,4 +1,5 @@
 import cx from "classnames";
+import Image from "next/image";
 import Link from "next/link";
 
 export interface LogoProps {
@@ -9,7 +10,8 @@ export interface LogoProps {
 export const Logo = ({ href = "/", className }: LogoProps) => {
   return (
     <Link href={href} className="flex items-center gap-3">
-      <span className="block h-7 w-7 rounded-full bg-primary" />
+      <Image src="/lenschool-logo.svg" height={40} width={40} alt="Lenschool" />
+      {/* <span className="block h-7 w-7 rounded-full bg-primary" /> */}
       <span className={cx("font-extrabold text-2xl", className)}>
         Lenschool
       </span>
