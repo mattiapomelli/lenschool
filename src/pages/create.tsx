9ffd1d06a1e66e2b62303b1a2f226d38306b1d5c
mattiapomelli <mattiapomelli@gmail.com>
2023-05-13 13:08:53 +0100

    Temporarily disable eslint rule

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -33,6 +33,7 @@ const CreateCourseForm = () => {
   const [video, setVideo] = useState<File | undefined>();
 
   const {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     mutateAsync: uploadVideo,
     error,
     progressFormatted,
