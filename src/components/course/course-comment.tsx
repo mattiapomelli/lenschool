7c2567729838d909e744449d9c4b4017e20fd227
mattiapomelli <mattiapomelli@gmail.com>
2023-05-13 02:06:20 +0100

    Show first comment of each comment

--- src/components/course/course-comment.tsx
+++ src/components/course/course-comment.tsx
@@ -1,15 +1,22 @@
-import { CommentWithFirstComment } from "@lens-protocol/react-web";
+import { Comment, CommentWithFirstComment } from "@lens-protocol/react-web";
+import cx from "classnames";
 import Image from "next/legacy/image";
 
 import { getPictureURL } from "@utils/ipfs-to-gateway-url";
 
-interface CourseCommentProps {
-  comment: CommentWithFirstComment;
+interface CourseCommentCardProps {
+  comment: Comment;
+  className?: string;
 }
 
-export const CourseComment = ({ comment }: CourseCommentProps) => {
+const CourseCommentCard = ({ comment, className }: CourseCommentCardProps) => {
   return (
-    <div className="rounded-box flex flex-col gap-2 bg-base-200 p-4">
+    <div
+      className={cx(
+        "rounded-box flex flex-col gap-2 bg-base-200 p-4",
+        className,
+      )}
+    >
       <div className="flex items-center gap-2">
         <div className="relative mt-2 h-10 w-10 shrink-0 overflow-hidden rounded-full">
           <Image
@@ -24,6 +31,21 @@ export const CourseComment = ({ comment }: CourseCommentProps) => {
       </div>
 
       <p className="text-base-content/70">{comment.metadata.content}</p>
+    </div>
+  );
+};
+
+interface CourseCommentProps {
+  comment: CommentWithFirstComment;
+}
+
+export const CourseComment = ({ comment }: CourseCommentProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <CourseCommentCard comment={comment} />
+      {comment.firstComment && (
+        <CourseCommentCard comment={comment.firstComment} className="ml-20" />
+      )}
     </div>
   );
 };
